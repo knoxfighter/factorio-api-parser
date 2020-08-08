70c1ce1814e1d6ce0871daf3be3b5f6175ebc74b
knoxfighter <knoxfighter@asdff45.de>
2020-08-08 02:08:32 +0200

    parse whole page(all versions) and save it to a directory each

--- src/main/java/moe/knox/factorio_api_parser/Main.java
+++ src/main/java/moe/knox/factorio_api_parser/Main.java
@@ -4,37 +4,62 @@ import moe.knox.factorio_api_parser.lua_types.Attribute;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) {
 		// Download and parse a single page
-		LuaApiParser.ParseOverviewResult parseOverviewResult = LuaApiParser.parseOverviewPageFromDownload("https://lua-api.factorio.com/latest/");
+		Map<String, LuaApiParser.ParseOverviewResult> overviewResultMap = LuaApiParser.parseVersionList();
 
 		// save the file
-		parseOverviewResult.classes.forEach((className, aClass) -> {
-			// open file to write to!
+		overviewResultMap.forEach((versionName, parseOverviewResult) -> {
+			String basePath = "luaOutput/" + versionName + "/";
+
 			try {
-				FileOutputStream outputStream = new FileOutputStream("test/" + className + ".lua");
-				aClass.saveToFile(outputStream);
+				Files.createDirectories(Paths.get("basePath"));
+			} catch (IOException e) {
+				e.printStackTrace();
+				return;
+			}
+
+			parseOverviewResult.classes.forEach((className, aClass) -> {
+				// open file to write to!
+				try {
+					FileOutputStream outputStream = new FileOutputStream(basePath + className + ".lua");
+					aClass.saveToFile(outputStream);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+
+			try {
+				FileOutputStream outputStream = new FileOutputStream(basePath + "globals.lua");
+				for (Attribute global : parseOverviewResult.globals) {
+					global.saveToFile(outputStream, "");
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		});
 
-		try {
-			FileOutputStream outputStream = new FileOutputStream("test/globals.lua");
-			for (Attribute global : parseOverviewResult.globals) {
-				global.saveToFile(outputStream, "");
+			try {
+				FileOutputStream outputStream = new FileOutputStream(basePath + "defines.lua");
+				parseOverviewResult.defines.saveToFile(outputStream);
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 
-		try {
-			FileOutputStream outputStream = new FileOutputStream("test/defines.lua");
-			parseOverviewResult.defines.saveToFile(outputStream);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			// copy in the hardcoded.lua file
+			try {
+				Files.copy(Paths.get(Main.class.getClassLoader().getResource("hardcoded.lua").toURI()), Paths.get(basePath + "hardcoded.lua"), StandardCopyOption.REPLACE_EXISTING);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 }
