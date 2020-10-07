1cfe57238af5e06f6c1cf84c273e7da3160ed720
knoxfighter <knoxfighter@asdff45.de>
2020-10-07 04:53:00 +0200

    also check types for updates && changed outcommented code to removing type properties

--- parser/prototype/src/main/java/moe/knox/prototype/Main.java
+++ parser/prototype/src/main/java/moe/knox/prototype/Main.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.*;
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 	static void deleteDirectory(Path path) throws IOException {
@@ -36,6 +38,27 @@ public class Main {
 						throw new RuntimeException(e);
 					}
 				});
+	}
+
+	private static void parseLinkElementsAndTheirPage(Path newPath, Elements allPrototypeLinks) throws IOException {
+		// generate unique list of links
+		Set<String> links = new HashSet<>();
+		for (Element allPrototypeLink : allPrototypeLinks) {
+			String href = allPrototypeLink.attr("href");
+			if (!href.equals("http://lua-api.factorio.com/latest/Concepts.html#Modifier")) {
+				links.add(href);
+			}
+		}
+
+		// iterate over the unique list
+		for (String link : links) {
+			Document subPage = Jsoup.connect("https://wiki.factorio.com" + link).get();
+			Element subContentText = subPage.getElementById("mw-content-text");
+			String subContentHtml = subContentText.html();
+			String subContentClean = Jsoup.clean(subContentHtml, "https://wiki.factorio.com" + link, new CustomHtmlWhitelist());
+			link = link.replace("/", "_");
+			Files.writeString(newPath.resolve(link + ".html"), subContentClean, StandardOpenOption.CREATE_NEW);
+		}
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException {
@@ -67,18 +90,13 @@ public class Main {
 		String mainContentClean = Jsoup.clean(mainContentHtml, "https://wiki.factorio.com/Prototype_overview", new CustomHtmlWhitelist());
 		Files.writeString(newPath.resolve("overview.html"), mainContentClean, StandardOpenOption.CREATE_NEW);
 
-		// download all prototype pages
+		// download and save all prototype pages
 		Elements allPrototypeLinks = mainPage.select(".prototype-toc-section-title > a:first-child");
-		for (Element allPrototypeLink : allPrototypeLinks) {
-			String href = allPrototypeLink.attr("href");
-			Document subPage = Jsoup.connect("https://wiki.factorio.com" + href).get();
-			Element subContentText = subPage.getElementById("mw-content-text");
-			String subContentHtml = subContentText.html();
-			String subContentClean = Jsoup.clean(subContentHtml, "https://wiki.factorio.com" + href, new CustomHtmlWhitelist());
-			href = href.replace("/", "_");
-			Files.writeString(newPath.resolve(href + ".html"), subContentClean, StandardOpenOption.CREATE_NEW);
-		}
+		parseLinkElementsAndTheirPage(newPath, allPrototypeLinks);
 
+		// download all type pages
+		Elements allTypeLinks = mainPage.select(".prototype-toc-item-info > a");
+		parseLinkElementsAndTheirPage(newPath, allTypeLinks);
 
 		// save to file named after current timestamp
 		String diffFileName = String.valueOf(System.currentTimeMillis());
@@ -105,106 +123,34 @@ public class Main {
 			embed.setTitle("Some changes to the prototype definitions happened");
 			embed.setDescription(String.format("https://factorio-api.knox.moe/wiki/%s", diffFileName));
 			discordWebhook.addEmbed(embed);
-			discordWebhook.execute();
+//			discordWebhook.execute();
 		}
 
 		// move everything from the new directory to the old directory
 		moveDirectory(newPath, oldPath);
 
+
+		////////////////////////////////////////////////
+		// remove all properties with the name "type" //
+		////////////////////////////////////////////////
+
 //		Map<String, Prototype> prototypes;
 //		// read prototypes from jsom file
-//		try (FileReader reader = new FileReader("prototypes.json")) {
-//			Type mapType = new TypeToken<Map<String, Prototype>>() { }.getType();
-//			prototypes = new GsonBuilder().create().fromJson(reader, mapType);
+//		try (FileReader prototypeJsonRead = new FileReader("prototypes.json")) {
+//			Type mapType = new TypeToken<Map<String, Prototype>>() {
+//			}.getType();
+//			prototypes = new GsonBuilder().create().fromJson(prototypeJsonRead, mapType);
 //		} catch (FileNotFoundException e) {
-//			prototypes = Parser.fullParser();
+//			e.printStackTrace();
+//			return;
 //		} catch (IOException e) {
 //			e.printStackTrace();
 //			return;
 //		}
 //
-//		Scanner scanner = new Scanner(System.in);
-
-		// description editor
-		/*try {
-			for (Map.Entry<String, Prototype> entry : prototypes.entrySet()) {
-				String s = entry.getKey();
-				Prototype prototype = entry.getValue();
-				if (prototype.description != null && prototype.description.isEmpty()) {
-					// no description yet, print link and ask for it
-					System.out.println("https://wiki.factorio.com" + prototype.link);
-					System.out.print("Description: ");
-					String desc = scanner.nextLine();
-					if (desc.equals("exit")) {
-						break;
-					} else {
-						prototype.description = desc.strip();
-					}
-				}
-
-				boolean breaker = false;
-				for (Property property : prototype.properties) {
-					boolean printed = false;
-					if (property.description != null && property.description.isEmpty()) {
-						// no description yet, print link and ask for it
-						System.out.println("https://wiki.factorio.com" + prototype.link + "#" + property.name);
-						printed = true;
-						System.out.print("Description: ");
-						if (prototype.id != null && (prototype.id.equals("utility-sprites") || prototype.id.equals("utility-sounds") || prototype.id.equals("utility-constants"))) {
-							property.description = "null";
-						} else {
-							String desc = scanner.nextLine();
-							if (desc.equals("exit")) {
-								breaker = true;
-								break;
-							} else {
-								property.description = desc.strip();
-							}
-						}
-					}
-					if (property._default != null && property._default.isEmpty()) {
-						// no default value yet, ask for it
-						if (!printed) {
-							System.out.println("https://wiki.factorio.com" + prototype.link + "#" + property.name);
-						}
-						System.out.print("Default: ");
-						if (prototype.id != null && (prototype.id.equals("utility-sprites") || prototype.id.equals("utility-sounds") || prototype.id.equals("utility-constants"))) {
-							property._default = "null";
-						} else {
-							String def = scanner.nextLine();
-							if (def.equals("exit")) {
-								breaker = true;
-								break;
-							} else {
-								property._default = def.strip();
-							}
-						}
-					}
-				}
-				if (breaker) {
-					break;
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}*/
-
-		// replace "null" with null and resave
-		/*for (Map.Entry<String, Prototype> entry : prototypes.entrySet()) {
-			String s = entry.getKey();
-			Prototype prototype = entry.getValue();
-
-			for (Property property : prototype.properties) {
-				if (property._default.equals("null")) {
-					property._default = null;
-				}
-
-				if (property.description.equals("null")) {
-					property.description = null;
-				}
-			}
-		}*/
-
+//		prototypes.forEach((s, prototype) -> prototype.properties.removeIf(property -> property.name.equals("type")));
+//
+//		// save json again
 //		try (Writer writer = new FileWriter("prototypes.json")) {
 //			new GsonBuilder().setPrettyPrinting().create().toJson(prototypes, writer);
 //		} catch (IOException e) {
