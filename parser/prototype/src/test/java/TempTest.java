e74b45e64216c745c780e8f35bd1e1655f43c76e
knoxfighter <knoxfighter@asdff45.de>
2021-10-12 12:45:41 +0200

    use unique names for PrototypeTypes. (Added "T_" as prefix)

--- parser/prototype/src/test/java/TempTest.java
+++ parser/prototype/src/test/java/TempTest.java
@@ -1,5 +1,5 @@
 import com.google.gson.GsonBuilder;
-import moe.knox.prototype.types.JsonRoot;
+import moe.knox.prototype.types.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
@@ -22,6 +22,8 @@ public class TempTest {
 			return;
 		}
 
+
+
 //		try (Writer writer = new FileWriter("../prototypes.json")) {
 //			new GsonBuilder()
 //					.setPrettyPrinting()
@@ -43,4 +45,41 @@ public class TempTest {
 			e.printStackTrace();
 		}
 	}
+
+//	String replaceRegex = "(?!nil)(?!boolean)(?!number)(?!table)(?!any)(?!string)(\\b\\w+\\b)";
+//
+//	for (Table table : prototypes.tables) {
+//		if (table.parent != null) {
+//			table.parent = table.parent.replaceAll("(\\w+)", "PT_$1");
+//		}
+//
+//		if (table.prototype != null) {
+//			if (table.prototype.isEmpty()) {
+//				table.prototype = null;
+//			} else {
+//				table.prototype = "PT_" + table.prototype;
+//			}
+//		}
+//
+//		table.name = "PT_" + table.name;
+//
+//		for (Property property : table.properties) {
+//			property.type = property.type.replaceAll(replaceRegex, "PT_$1");
+//		}
+//	}
+//
+//		for (Alias alias : prototypes.aliases) {
+//		alias.name = "PT_" + alias.name;
+//
+//		alias.other = alias.other.replaceAll(replaceRegex, "PT_$1");
+//	}
+//
+//		for (StringType string : prototypes.strings) {
+//		string.name = string.name.replaceAll(replaceRegex, "PT_$1");
+//	}
+//
+//		for (StringType prototype : prototypes.prototypes) {
+//		prototype.name = "PT_" + prototype.name;
+//		prototype.value.replaceAll((key, value) -> value.replaceAll(replaceRegex, "PT_$1"));
+//	}
 }
